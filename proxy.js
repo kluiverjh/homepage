b69a5b432f5d20d58c3df509a5dd53fd413bc3bf
Hein Kluiver <hein.kluiver@tno.nl>
2019-11-19 13:46:30 +0100

    Bug fixes. The copper websocket and geofencer are still work in progress

--- proxy.js
+++ proxy.js
@@ -18,17 +18,20 @@ const time = process.env.time; // || 'localhost:8100';
 const lfs = process.env.lfs; // || 'localhost:9090';
 const copper = process.env.copper; // || 'localhost:8088';
 const copper_api = process.env.copper_api; // || 'localhost:3008';
-const replayservice = process.env.replayservice; // || 'localhost:8080';
-const replayservice_api = process.env.replayservice_api; // || 'localhost:8209';
+const replayservice = process.env.replay_service; // || 'localhost:8080';
+const replayservice_api = process.env.replay_service_api; // || 'localhost:8209';
 
 const useSsl = process.env.ssl || false;
 
+const replayservice_subdomain = process.env.subdomain_replayer || 'replay-service';
+const replayservice_api_subdomain = process.env.subdomain_replayer_api || 'replay-service-api';
+const copper_subdomain = process.env.subdomain_copper ||'copper';
+const copper_api_subdomain = process.env.subdomain_copper_api ||'copper_api';
 
-const replayservice_subdomain = 'replayservice';
 const replayservice_subdomain_full = `${replayservice_subdomain}.${hostname}`.toLocaleLowerCase();
-
-const replayservice_api_subdomain = 'replayservice_api';
 const replayservice_api_subdomain_full = `${replayservice_api_subdomain}.${hostname}`.toLocaleLowerCase();
+const copper_subdomain_full = `${copper_subdomain}.${hostname}`.toLocaleLowerCase();
+const copper_api_subdomain_full = `${copper_api_subdomain}.${hostname}`.toLocaleLowerCase();
 
 
 // Title on the home page
@@ -56,6 +59,7 @@ app.use(cors());
 app.use(async function (ctx, next) {
   // Redirect /replayservice/ to http://replayservice.<domain>
   if (ctx.path === `/replayservice/`) ctx.redirect('http://' +  replayservice_subdomain_full) ;
+  if (ctx.path === `/copper/`) ctx.redirect('http://' +  copper_subdomain_full) ;
   // The homepage GUI needs to get the current configuration
   if (ctx.method !== 'GET' || ctx.path !== '/services') return await next();
   ctx.type = 'json';
@@ -84,8 +88,8 @@ console.log(`${(useSsl) ? 'SSL is enabled' : 'SSL is not enabled'}`);
 
 
 if (schemas) {
-  console.log(`Map '${hostname}/schemas/*' --> '${schemas}/*'`);
-  console.log(`Map '${hostname}/api/schema-registry/*' --> '${schemas}/api/schema-registry/*'`);
+  console.log(`Map external '${hostname}/schemas/*' --> to internal '${schemas}/*'`);
+  console.log(`Map external '${hostname}/api/schema-registry/*' --> to internal '${schemas}/api/schema-registry/*'`);
   const resolver = function (host, url, req) {
     if (/^\/schemas\//.test(url) || /\/api\/schema-registry/.test(url)) {
       req.url = url.replace(/^\/schemas/, '');
@@ -97,8 +101,8 @@ if (schemas) {
 }
 
 if (topics) {
-  console.log(`Map '${hostname}/topics/*' --> '${topics}/*'`);
-  console.log(`Map '${hostname}/api/kafka-rest-proxy/*' --> '${topics}/api/kafka-rest-proxy/*'`);
+  console.log(`Map external '${hostname}/topics/*' --> '${topics}/*'`);
+  console.log(`Map external '${hostname}/api/kafka-rest-proxy/*' --> to internal '${topics}/api/kafka-rest-proxy/*'`);
   const resolver = function (host, url, req) {
     if (/^\/topics\//.test(url) || /\/api\/kafka-rest-proxy/.test(url)) {
       req.url = url.replace(/^\/topics/, '');
@@ -111,9 +115,9 @@ if (topics) {
 } else console.log(`No proxy for topic website`);
 
 if (admin) {
-  console.log(`Map '${hostname}/admin/*' --> '${admin}/*'`);
-  console.log(`Map '${hostname}/AdminServiceWSEndpoint/*' --> '${topics}/AdminServiceWSEndpoint/*'`);
-  console.log(`Map '${hostname}/AdminService/*' --> '${topics}/AdminService/*'`);
+  console.log(`Map external '${hostname}/admin/*' --> to internal '${admin}/*'`);
+  console.log(`Map external '${hostname}/AdminServiceWSEndpoint/*' --> to internal  '${topics}/AdminServiceWSEndpoint/*'`);
+  console.log(`Map external '${hostname}/AdminService/*' --> to internal '${topics}/AdminService/*'`);
   const resolver = function (host, url, req) {
     if (/^\/admin\//.test(url) || /^\/AdminServiceWSEndpoint/.test(url) || /^\/AdminService/.test(url)) {
       req.url = url.replace(/^\/admin/, '');
@@ -125,11 +129,11 @@ if (admin) {
 } else console.log(`No proxy for admin tool`);
 
 if (aar) {
-  console.log(`Map '${hostname}/aar/*' --> '${aar}'`);
-  console.log(`Map '${hostname}/static/js/*' --> '${aar}/static/js/*'`);
-  console.log(`Map '${hostname}/AARService/*' --> '${aar}/AARService/*'`);
-  console.log(`Map '${hostname}/static/img/*' --> '${aar}/static/img/*'`);
-  console.log(`Map '${hostname}/static/css/*' --> '${aar}/static/css/*'`);
+  console.log(`Map external '${hostname}/aar/*' --> to internal '${aar}'`);
+  console.log(`Map external '${hostname}/static/js/*' --> to internal '${aar}/static/js/*'`);
+  console.log(`Map external '${hostname}/AARService/*' --> to internal '${aar}/AARService/*'`);
+  console.log(`Map external '${hostname}/static/img/*' --> to internal '${aar}/static/img/*'`);
+  console.log(`Map external '${hostname}/static/css/*' --> to internal '${aar}/static/css/*'`);
   const resolver = function (host, url, req) {
     if (
       /^\/aar\//.test(url) ||
@@ -145,27 +149,6 @@ if (aar) {
   resolver.priority = 100;
   resolvers.push(resolver);
 } else console.log(`No proxy for after action review.`);
-
-if (copper) {
-  console.log(`Map '${hostname}/copper/*' --> '${copper}'`);
-  console.log(`Map '${hostname}/sockjs-node/*' --> '${copper}/sockjs-node/*'`);
-
-  const resolver = function (host, url, req) {
-    // const guid_regex = /^\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i;  // Check for /<guid> in url
-    // blob:http://localhost/<guid> is generated by javascript and refers to local browser cache
-    const copper_regex = /^\/copper\//; // Check for /copper/ in url
-    const sockjs_regex = /^\/sockjs-node\//; // Check for /sockjs-node/ in url
-
-    if (copper_regex.test(url) || sockjs_regex.test(url)) {
-      req.url = url.replace(/^\/copper/, '');
-      return { url: `${copper}/` };
-    }
-
-
-  };
-  resolver.priority = 100;
-  resolvers.push(resolver);
-} else console.log(`No proxy for copper configured.`);
 
 const proxy = require('redbird')(
   useSsl
@@ -184,10 +167,9 @@ const proxy = require('redbird')(
 );
 
 
-const copper_api_subdomain = 'copper_api';
-const copper_api_subdomain_full = `${copper_api_subdomain}.${hostname}`.toLocaleLowerCase();
+
 if (copper) {
-  console.log(`Map '${copper_api_subdomain_full}/*' --> '${copper_api}'`);
+  
     // Redirect COPPER REST API url
     // An embedded REST url is used in the vue copper web application to connect to the server.
     // When this url is configured for the reserve proxy (e.g. http://<proxy_domain>/copper_api),
@@ -197,43 +179,46 @@ if (copper) {
     // Fot the reverse proxy it is ambigue where to redirect the stream if there are multiple websockets
     // Using a subdomain is easiest solution to fix this problem
     // Not tested under SSL (subdomain requires own certificate?)
-  proxy.register(`${copper_api_subdomain_full}`, `${copper_api}`);
-}
+	console.log(`Map external '${copper_subdomain_full}/*' --> to internal '${copper}'`);
+	console.log(`Map external '${copper_api_subdomain_full}/*' --> to internal '${copper_api}'`);
+	proxy.register(`${copper_subdomain_full}`, `${copper}`);
+    proxy.register(`${copper_api_subdomain_full}`, `${copper_api}`);
+} else console.log(`No proxy for copper configured.`);
 
 
 if (replayservice) {
    // All url's in web app are relative, use subdomain to prevent resolve all url's (e.g. app.*.js can be used by multiple applications)
    // Examples: app.<id>.css, chunk-vendors.<id>.css, app.<id>.js
    // Create subdomain for web-site and subdomain for rest calls (use different ports)
-  console.log(`Map '${replayservice_subdomain_full}/*' --> '${replayservice}'`);
+  console.log(`Map external '${replayservice_subdomain_full}/*' --> to internal '${replayservice}'`);
+  console.log(`Map external '${replayservice_api_subdomain_full}/*' --> to internal '${replayservice_api}'`);
   proxy.register(`${replayservice_subdomain_full}`, `${replayservice}`);
-  console.log(`Map '${replayservice_api_subdomain_full}/*' --> '${replayservice_api}'`);
   proxy.register(`${replayservice_api_subdomain_full}`, `${replayservice_api}`);
 }
 
 if (tmt) {
-  console.log(`Map '${hostname}/tmt/*' --> '${tmt}/*'`);
-  console.log(`Map '${hostname}/socket.io' --> '${tmt}/socket.io'`);
+  console.log(`Map external '${hostname}/tmt/*' --> to internal '${tmt}/*'`);
+  console.log(`Map external '${hostname}/socket.io' --> to internal '${tmt}/socket.io'`);
   proxy.register(`${hostname}/tmt`, `${tmt}`);
   proxy.register(`${hostname}/socket.io`, `${tmt}/socket.io`);
 } else console.log(`No proxy for trail management tool configured.`);
 
 if (rest) {
-  console.log(`Map '${hostname}/rest' --> '${rest}'`);
-  proxy.register(`${hostname}/rest`, `${rest}`);
+  console.log(`Map external '${hostname}/rest' --> '${rest}'`);
+  proxy.register(`external ${hostname}/rest`, `${rest}`);
 } else console.log(`No proxy for rest configured.`);
 
 // Timeservice uses subpath /time-service' (and websocket with subpath '/time-service/socket.io')
 // Therefore no subdomain is needed (the service is reserve proxy safe)
 if (time) {
-  console.log(`Map '${hostname}/time' --> '${time}/time-service'`);
-  console.log(`Map '${hostname}/time-service' --> '${time}/time-service'`);
-  proxy.register(`${hostname}/time`, `${time}/time-service`);
-  proxy.register(`${hostname}/time-service`, `${time}/time-service`);
+  console.log(`Map external '${hostname}/time' --> to internal '${time}/time-service'`);
+  console.log(`Map external '${hostname}/time-service' --> to internal '${time}/time-service'`);
+  proxy.register(`${hostname}/time`, `${time}`);
+  proxy.register(`${hostname}/time-service`, `${time}`);
 } else console.log(`No proxy for time service configured.`);
 
 if (lfs) {
-  console.log(`Map '${hostname}/lfs/*' --> '${lfs}/*'`);
+  console.log(`Map external '${hostname}/lfs/*' --> to internal '${lfs}/*'`);
   proxy.register(`${hostname}/lfs`, `${lfs}`);
 } else console.log(`No proxy for large file service configured.`);
 
